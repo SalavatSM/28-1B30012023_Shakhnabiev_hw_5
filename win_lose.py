0493ec406771b9e85620f687a510cbc44198114b
SalavatSM <salavatfromkg@gmail.com>
2023-03-23 18:46:07 +0600

    v.1

--- win_lose.py
+++ win_lose.py
@@ -5,34 +5,40 @@ from decouple import config
 def casino():
     my_money = config('MY_MONEY', cast=int)
     balance = my_money
-    while True:
+    while balance > 0:
         win_slot = one_to_thirty(1, 31)
-        chose_slot = int(input('Выберите слот '))
-        if chose_slot in range(1, 31):
-            print('Слот выбран. Делайте ставку.')
+        chose_slot = int(input('Select your slot between 1 and 30\n'))
+        while chose_slot > 30 or chose_slot < 1:
+            print('It does not have slot with such a number.')
+            chose_slot = int(input('Select slot between 1 and 30\n'))
         else:
-            print('Выберите слот от 1 до 30')
-        bet = int(input('Выберите ставка '))
-        if balance < bet:
-            print(f'У вас столько денег на балансе. У вас на балансе: {balance}')
+            print(f'Slot number {chose_slot} selected')
+        bet = int(input('Place your bet!\n'))
+        while bet > balance:
+            print(f'You do not have enough money to bet so much money. Your balance is: {balance}')
+            print('Place your bet not much than your balance.')
+            bet = int(input('Place your bet!\n'))
         else:
-            print('Ставка сделана!')
+            print('Bet is placed!')
         if win_slot == chose_slot:
             balance += bet * 2
-            print(f'You won ${bet * 2} !!!, Your balance: {balance}')
+            print(f'Your bet has won!!!\nYou won ${bet * 2} !!!\nYour balance: {balance}')
         elif win_slot != chose_slot:
             balance -= bet
-            print(f'Your bet not win. Your balance: {balance}')
+            print(f'Your bet is not win. Your balance: {balance}')
 
         if balance > 0:
-            a = input('Do you wanna bet again? Yes/No').lower()
-            if a == 'yes':
-                continue
-            elif a == 'no':
-                print(f'Your balance: {balance}')
-                break
+            answer = input('Do you wanna keep playing? Yes/No\n').lower()
+            while answer != 'Yes'.lower() and answer != 'No'.lower():
+                answer = input('Do you wanna keep playing? Yes/No\n').lower()
             else:
-                break
+                if answer == 'Yes'.lower():
+                    continue
+                elif answer == 'No'.lower():
+                    print(f'Your balance: {balance}.\nGlad to see you again!')
+                    break
+    else:
+        print('We are glad to see you again with your money!')
 
 
 if __name__ == '__main__':
